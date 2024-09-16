aff5a58405ecd66ea9426ae21a2643157a5af88c
NethmiHasi <nethuhasi2001@gmail.com>
2024-09-17 06:28:51 +0700

    user model

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,10 @@
-const express = require('express');
+import express from 'express';
+import dotenv from 'dotenv';
+import authRoutes from './routes/auth.routes.js';
+import connectToMongoDB from './db/connectToMongoDB.js';
 
 const app = express();
+dotenv.config(); 
 const PORT = process.env.PORT || 5000;
 
 app.get("/",(req,res)=>{
@@ -8,7 +12,11 @@ app.get("/",(req,res)=>{
     res.send('Hello World');
 
 });
+//middleware 
+app.use("/api/auth",authRoutes);
 
-app.listen(5000,()=>{
-    console.log('Server is running on port 5000');
+
+app.listen(PORT,()=>{
+    connectToMongoDB();
+    console.log(`Server is running on port ${PORT}`);
 })
