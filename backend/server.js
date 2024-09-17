e3a9d61d086508458970f97e54d5545ef3a8d9d5
NethmiHasi <nethuhasi2001@gmail.com>
2024-09-17 10:30:40 +0700

    authentication completed

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,26 @@
 import express from 'express';
 import dotenv from 'dotenv';
+
 import authRoutes from './routes/auth.routes.js';
 import connectToMongoDB from './db/connectToMongoDB.js';
 
 const app = express();
-dotenv.config(); 
 const PORT = process.env.PORT || 5000;
 
-app.get("/",(req,res)=>{
-    //root route http://localhost:5000/
-    res.send('Hello World');
+dotenv.config(); 
 
-});
+app.use(express.json()); //middleware to parse json data(from request body)
+
 //middleware 
 app.use("/api/auth",authRoutes);
+
+// app.get("/",(req,res)=>{
+//     //root route http://localhost:5000/
+//     res.send('Hello World');
+
+// });
+
+
 
 
 app.listen(PORT,()=>{
